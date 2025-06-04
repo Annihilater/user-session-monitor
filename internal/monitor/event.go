e445708c6563c78e4cc2e27cd5ce28968ec75c00
Annihilater <9960889@gmail.com>
2025-06-04 15:19:52 +0800

    Feature: 新增事件类型和事件总线实现，支持事件的发布与订阅

---
+++ internal/monitor/event.go
@@ -0,0 +1,47 @@
+package monitor
+
+import (
+	"time"
+
+	"github.com/Annihilater/user-session-monitor/internal/types"
+)
+
+// EventType 定义事件类型
+type EventType int
+
+const (
+	EventTypeLogin EventType = iota
+	EventTypeLogout
+)
+
+// Event 定义事件结构
+type Event struct {
+	Type       EventType
+	Username   string
+	IP         string
+	Port       string
+	Timestamp  time.Time
+	ServerInfo *types.ServerInfo
+}
+
+// EventBus 事件总线
+type EventBus struct {
+	eventChan chan Event
+}
+
+// NewEventBus 创建新的事件总线
+func NewEventBus(bufferSize int) *EventBus {
+	return &EventBus{
+		eventChan: make(chan Event, bufferSize),
+	}
+}
+
+// Publish 发布事件
+func (eb *EventBus) Publish(event Event) {
+	eb.eventChan <- event
+}
+
+// Subscribe 订阅事件
+func (eb *EventBus) Subscribe() <-chan Event {
+	return eb.eventChan
+}
