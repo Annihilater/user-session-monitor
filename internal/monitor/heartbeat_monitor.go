a0e058672fd39c1ab39631bccb8c5916b5bdc041
Annihilater <9960889@gmail.com>
2025-06-05 14:03:39 +0800

    Refactor: 重构心跳监控器，整合基础监控器逻辑，优化启动和停止流程

--- internal/monitor/heartbeat_monitor.go
+++ internal/monitor/heartbeat_monitor.go
@@ -1,8 +1,6 @@
 package monitor
 
 import (
-	"runtime"
-	"sync"
 	"time"
 
 	"go.uber.org/zap"
@@ -10,64 +8,48 @@ import (
 
 // HeartbeatMonitor 心跳监控器
 type HeartbeatMonitor struct {
-	logger   *zap.Logger
-	interval time.Duration
-	stopChan chan struct{}
-	wg       sync.WaitGroup
-	runMode  string // 运行模式：thread 或 goroutine
+	BaseMonitor
 }
 
 // NewHeartbeatMonitor 创建新的心跳监控器
 func NewHeartbeatMonitor(logger *zap.Logger, interval time.Duration, runMode string) *HeartbeatMonitor {
 	return &HeartbeatMonitor{
-		logger:   logger,
-		interval: interval,
-		stopChan: make(chan struct{}),
-		runMode:  runMode,
+		BaseMonitor: NewBaseMonitor("心跳监控", logger, interval, runMode),
 	}
 }
 
 // Start 启动心跳监控
 func (hm *HeartbeatMonitor) Start() {
-	hm.wg.Add(1)
-	hm.logger.Info("启动心跳监控",
-		zap.String("run_mode", hm.runMode),
-	)
-	if hm.runMode == "thread" {
-		go func() {
-			runtime.LockOSThread()
-			defer runtime.UnlockOSThread()
-			hm.monitor()
-		}()
-	} else {
-		go hm.monitor()
-	}
+	hm.BaseMonitor.Start(hm.monitor)
 }
 
 // Stop 停止心跳监控
 func (hm *HeartbeatMonitor) Stop() {
-	close(hm.stopChan)
-	hm.wg.Wait()
+	hm.BaseMonitor.Stop()
 }
 
 // monitor 心跳监控主循环
 func (hm *HeartbeatMonitor) monitor() {
-	defer hm.wg.Done()
-	ticker := time.NewTicker(hm.interval)
+	defer hm.Done()
+	ticker := time.NewTicker(hm.GetInterval())
 	defer ticker.Stop()
 
 	// 记录启动时间
 	startTime := time.Now()
 
 	for {
+		if hm.IsStopped() {
+			return
+		}
+
 		select {
 		case <-hm.stopChan:
 			return
 		case <-ticker.C:
 			uptime := time.Since(startTime)
-			hm.logger.Info("监控程序心跳",
+			hm.GetLogger().Info("监控程序心跳",
 				zap.Duration("uptime", uptime),
-				zap.Duration("interval", hm.interval),
+				zap.Duration("interval", hm.GetInterval()),
 			)
 		}
 	}
