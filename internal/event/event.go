678aeed40d0b0a1c49fef4ee171d289b9e373ce9
Annihilater <9960889@gmail.com>
2025-06-05 15:51:13 +0800

    Refactor: 修改事件类型和事件总线结构，提升代码一致性和可维护性

--- internal/event/event.go
+++ internal/event/event.go
@@ -1,47 +1,30 @@
 package event
 
 import (
-	"time"
-
 	"github.com/Annihilater/user-session-monitor/internal/types"
 )
 
-// EventType 定义事件类型
-type EventType int
+// Type 定义事件类型
+type Type int
 
-const (
-	EventTypeLogin EventType = iota
-	EventTypeLogout
-)
-
-// Event 定义事件结构
-type Event struct {
-	Type       EventType
-	Username   string
-	IP         string
-	Port       string
-	Timestamp  time.Time
-	ServerInfo *types.ServerInfo
-}
-
-// EventBus 事件总线
-type EventBus struct {
+// Bus 事件总线
+type Bus struct {
 	eventChan chan types.Event
 }
 
-// NewEventBus 创建新的事件总线
-func NewEventBus(bufferSize int) *EventBus {
-	return &EventBus{
+// NewBus 创建新的事件总线
+func NewBus(bufferSize int) *Bus {
+	return &Bus{
 		eventChan: make(chan types.Event, bufferSize),
 	}
 }
 
 // Publish 发布事件
-func (eb *EventBus) Publish(event types.Event) {
+func (eb *Bus) Publish(event types.Event) {
 	eb.eventChan <- event
 }
 
 // Subscribe 订阅事件
-func (eb *EventBus) Subscribe() <-chan types.Event {
+func (eb *Bus) Subscribe() <-chan types.Event {
 	return eb.eventChan
 }
