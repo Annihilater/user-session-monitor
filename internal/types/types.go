a9ddec0753b8f7f1c96124272dd79ee8dc6c8ce2
Annihilater <9960889@gmail.com>
2025-06-05 15:27:50 +0800

    Refactor: 新增登录记录和事件结构，增强服务器信息管理功能

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,8 +1,36 @@
 package types
 
+import "time"
+
 // ServerInfo 服务器信息
 type ServerInfo struct {
 	Hostname string
 	IP       string
-	OSType   string // 操作系统类型
+	OSType   string
 }
+
+// LoginRecord 存储单个登录会话的详细信息
+type LoginRecord struct {
+	Username      string    // 用户名
+	Ip            string    // 登录源 IP
+	Port          string    // 登录源端口
+	LastLoginTime time.Time // 最近一次登录时间
+}
+
+// Event 定义事件结构
+type Event struct {
+	Type       EventType
+	Username   string
+	IP         string
+	Port       string
+	Timestamp  time.Time
+	ServerInfo *ServerInfo
+}
+
+// EventType 定义事件类型
+type EventType int
+
+const (
+	EventTypeLogin EventType = iota
+	EventTypeLogout
+)
