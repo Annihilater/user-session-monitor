42d074c4d02e550a1eff37845047fdedbc559d0d
Annihilater <9960889@gmail.com>
2025-06-05 22:11:17 +0800

    Feature: 增强事件总线功能，支持多订阅者管理和取消订阅逻辑

--- internal/event/event.go
+++ internal/event/event.go
@@ -1,6 +1,8 @@
 package event
 
 import (
+	"sync"
+
 	"github.com/Annihilater/user-session-monitor/internal/types"
 )
 
@@ -9,22 +11,55 @@ type Type int
 
 // Bus 事件总线
 type Bus struct {
-	eventChan chan types.Event
+	subscribers []chan types.Event
+	mu          sync.RWMutex
 }
 
 // NewBus 创建新的事件总线
 func NewBus(bufferSize int) *Bus {
 	return &Bus{
-		eventChan: make(chan types.Event, bufferSize),
+		subscribers: make([]chan types.Event, 0),
 	}
 }
 
 // Publish 发布事件
 func (eb *Bus) Publish(event types.Event) {
-	eb.eventChan <- event
+	eb.mu.RLock()
+	defer eb.mu.RUnlock()
+
+	// 向所有订阅者发送事件
+	for _, ch := range eb.subscribers {
+		// 使用非阻塞发送，避免一个订阅者阻塞其他订阅者
+		select {
+		case ch <- event:
+		default:
+			// 如果通道已满，跳过这个订阅者
+		}
+	}
 }
 
 // Subscribe 订阅事件
 func (eb *Bus) Subscribe() <-chan types.Event {
-	return eb.eventChan
+	ch := make(chan types.Event, 100) // 为每个订阅者创建一个带缓冲的通道
+
+	eb.mu.Lock()
+	eb.subscribers = append(eb.subscribers, ch)
+	eb.mu.Unlock()
+
+	return ch
+}
+
+// Unsubscribe 取消订阅
+func (eb *Bus) Unsubscribe(ch <-chan types.Event) {
+	eb.mu.Lock()
+	defer eb.mu.Unlock()
+
+	for i, subCh := range eb.subscribers {
+		if subCh == ch {
+			// 从订阅者列表中移除
+			eb.subscribers = append(eb.subscribers[:i], eb.subscribers[i+1:]...)
+			close(subCh)
+			break
+		}
+	}
 }
